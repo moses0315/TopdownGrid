7f6fd64a6475573d4f650adb0918bfd5c830e6a6
moses0315 <moses315@naver.com>
2024-07-14 19:58:33 +0900

    asset seting

--- player.gd
+++ player.gd
@@ -1,40 +1,3 @@
-#extends CharacterBody2D
-
-#@onready var ray = $RayCast2D
-#var grid_size = 128
-#var inputs = {
-	#"ui_up": Vector2.UP,
-	#"ui_down": Vector2.DOWN,
-	#"ui_left": Vector2.LEFT,
-	#"ui_right": Vector2.RIGHT
-#}
-#var is_moving = false
-#var attacking = false
-
-#func _physics_process(delta):
-	##for dir in inputs.keys():
-		##if Input.is_action_pressed(dir) and not is_moving:
-			##is_moving = true
-			##$Timer.start()
-			##move(dir)
-	#
-	#if Input.is_action_pressed("ui_up"):
-		#velocity.y -=1
-		#direction 
-	#
-	#if not attacking:
-		#attack()
-		#
-#func move(dir):
-	#var vector_pos = inputs[dir] * grid_size
-	#ray.target_position = vector_pos
-	#ray.force_raycast_update()
-	#if not ray.is_colliding():
-		#position += vector_pos
-#
-
-	
-
 extends CharacterBody2D
 
 @export var speed = 2
@@ -93,29 +56,34 @@ func _process(_delta):
 
 func move(dir):
 	if movement_direction != "still":
+		
 		ray.target_position = inputs[dir] * grid_size
 		ray.force_raycast_update()
 		if !ray.is_colliding():
 			moving = true
 			position += inputs[dir] * grid_size
-			await get_tree().create_timer(0.2).timeout
+			await get_tree().create_timer(0.3).timeout
 			moving = false
 
 func attack():
-	if Input.is_action_pressed("attackup"):
-		$AttackArea.position = Vector2(0, -grid_size)
-		$AttackArea/AnimationPlayer.play("attack")
-		attacking = true
-	if Input.is_action_pressed("attackdown"):
-		$AttackArea.position = Vector2(0, grid_size)
-		$AttackArea/AnimationPlayer.play("attack")
-		attacking = true
-	if Input.is_action_pressed("attackleft"):
-		$AttackArea.position = Vector2(-grid_size, 0)
-		$AttackArea/AnimationPlayer.play("attack")
-		attacking = true
-	if Input.is_action_pressed("attackright"):
-		$AttackArea.position = Vector2(grid_size, 0)
+	#if Input.is_action_pressed("attackup"):
+		#$AttackArea.position = Vector2(0, -grid_size)
+		#$AttackArea/AnimationPlayer.play("attack")
+		#attacking = true
+	#if Input.is_action_pressed("attackdown"):
+		#$AttackArea.position = Vector2(0, grid_size)
+		#$AttackArea/AnimationPlayer.play("attack")
+		#attacking = true
+	#if Input.is_action_pressed("attackleft"):
+		#$AttackArea.position = Vector2(-grid_size, 0)
+		#$AttackArea/AnimationPlayer.play("attack")
+		#attacking = true
+	#if Input.is_action_pressed("attackright"):
+		#$AttackArea.position = Vector2(grid_size, 0)
+		#$AttackArea/AnimationPlayer.play("attack")
+		#attacking = true
+	if Input.is_action_pressed("attack"):
+		$AttackArea.visible = true
 		$AttackArea/AnimationPlayer.play("attack")
 		attacking = true
 
